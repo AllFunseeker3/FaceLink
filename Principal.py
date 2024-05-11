e55e0e7ac4e1a0913b2802017f74bf7c47be03c2
Ricardo <moncraft02b1@hotmail.com>
2024-05-11 15:28:42 -0600

    Se agrega pantalla principal

---
+++ Principal.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from tkinter import ttk
+
+class Inicio:
+    def __init__(self):
+        self.ventana=Tk()
+        self.ventana.geometry("400x500")
+        self.ventana.title("Inicio")
+        
+        mainloop()
+
+        
+
+Inicio()
