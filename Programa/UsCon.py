b82967e414607751572bc007c3cca55cb9f7e10c
Ricardo <moncraft02b1@hotmail.com>
2024-05-17 14:00:37 -0600

    Actualizacion de la interfaz

--- Programa/UsCon.py
+++ Programa/UsCon.py
@@ -4,10 +4,10 @@ from tkinter import ttk
 class Inicio:
     def __init__(self):
         self.ventana=Tk()
-        self.ventana.geometry("400x500")
+        self.ventana.geometry("500x600")
         self.ventana.title("Inicio")
         
-        fondo="#FFF7F1"
+        fondo="#91C8E4"
         
         self.frame_sup=Frame(self.ventana)
         self.frame_sup.configure(bg=fondo)
@@ -16,9 +16,6 @@ class Inicio:
         self.frame_inf=Frame(self.ventana)
         self.frame_inf.configure(bg=fondo)
         self.frame_inf.pack(fill="both", expand=True)
-        
-        self.frame_inf.columnconfigure(0, weight=1)
-        self.frame_inf.columnconfigure(1, weight=1)
         
         #------------------#
         self.titulo=Label(self.frame_sup,
@@ -29,49 +26,49 @@ class Inicio:
         
         self.label_usuario = Label(self.frame_inf,
                                 text="Usuario",
-                                font=("Poppins", 10, "bold"),
+                                font=("Poppins", 14, "bold"),
                                 bg=fondo,
                                 fg="black")
-        self.label_usuario.grid(row=0, column=0,padx=1, sticky="e")
+        self.label_usuario.place(x=90,y=6,width=70,height=19)
         
         self.BoxUsuario=Entry(self.frame_inf,
                             bd=0,
                             width=25,
-                            font=("Poppins", 10))
-        self.BoxUsuario.grid(row=1, column=0,columnspan=3,padx=10, sticky="s")
+                            font=("Poppins", 15))
+        self.BoxUsuario.place(x=90,y=25,width=310,height=35)
         
         
         self.label_contrasena = Label(self.frame_inf,
                                    text="Contraseña",
-                                   font=("Poppins", 10, "bold"),
+                                   font=("Poppins", 14, "bold"),
                                    bg=fondo,
                                    fg="black")
-        self.label_contrasena.grid(row=4, column=0,pady=1, sticky="e")
+        self.label_contrasena.place(x=90,y=60,width=110,height=19)
         
         self.Boxcontrasena=Entry(self.frame_inf,
                               bd=0,
                               width=25,
-                              font=("Poppins", 10),
+                              font=("Poppins", 15),
                               show="*")
-        self.Boxcontrasena.grid(row=5, column=0,columnspan=3,padx=10, sticky="s")
+        self.Boxcontrasena.place(x=90,y=79,width=310,height=35)
         
         self.botonIngresar=Button(self.frame_inf,
                                   text="Ingresar",
                                   width=7,
                                   height=0,
                                   font=("Poppins",12))
-        self.botonIngresar.grid(row=6, column=1,columnspan=2, pady=30, sticky="n")
+        self.botonIngresar.place(x=300,y=118,width=100,height=30)
         
         self.botonSalir=Button(self.frame_inf,
                                   text="Salir",
                                   width=7,
                                   height=0,
                                   font=("Poppins",12))
-        self.botonSalir.grid(row=6, column=0,columnspan=2, pady=30, sticky="n")
+        self.botonSalir.place(x=90,y=118,width=100,height=30)
         
         
         mainloop()
 
-        
+
 
 Inicio()
