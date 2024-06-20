89fe1b0d1b465e1d11eb1c6f71a85a9b79e5bfdc
Cer0n <comcesar1@gmail.com>
2024-06-20 17:54:51 -0600

    se añadieron funciones escenciales a la libreria

--- ProgramaCTK/Librerias/LibreriaBaseDatos.py
+++ ProgramaCTK/Librerias/LibreriaBaseDatos.py
@@ -1,5 +1,6 @@
+
 import mysql.connector
-from mysql.connector import Error
+from mysql.connector import *
 
 def conectar_mysql(host, database, usuario, contraseña):
     try:
@@ -25,6 +26,7 @@ def ejecutar_consulta(consulta):
             cursor = connection.cursor()
             cursor.execute(consulta)
             resultados = cursor.fetchall()
+            resultados = [list(fila) for fila in resultados]
             return resultados
 
     except Error as e:
@@ -37,8 +39,36 @@ def ejecutar_consulta(consulta):
             connection.close()
             print("La conexión MySQL se ha cerrado")
 
-# Ejemplo de uso:
-resultados = ejecutar_consulta("SELECT * FROM Empleados")
-if resultados:
-    for fila in resultados:
-        print(fila)
+
+            
+def ejecutar_procedimiento(procedimiento, args=None):
+    conexion = conectar_mysql('localhost', 'FaceLink', 'root', '')
+    cursor = conexion.cursor()
+    try:
+            # Llamar al procedimiento almacenado
+            cursor.callproc(procedimiento, args=())
+            
+            # Si el procedimiento almacenado devuelve resultados, puedes obtenerlos así:
+            for result in cursor.stored_results():
+                #print(result.fetchall())
+                resultados = result.fetchall()
+                resultados = [list(fila) for fila in resultados]
+                return resultados
+            # Confirmar los cambios en la base de datos
+            conexion.commit()
+            print("Procedimiento almacenado ejecutado correctamente.")
+            
+    except Exception as e:
+            print(f"Error al ejecutar el procedimiento almacenado: {str(e)}")
+            
+    finally:
+            # Cerrar cursor y conexión
+            cursor.close()
+            conexion.close()
+
+ ##ejemplo de uso consulta
+       # for fila in ejecutar_consulta("select * from empleados"):
+        #    datos.append(fila)
+#Procedimiento
+       # for fila in ejecutar_procedimiento("ObtenerDatosEmpleados"):
+        #    datos.append(fila)
